2f77918a174b8b1c2e06d9c62ea3eae1591f2482
killed <56557003+killed@users.noreply.github.com>
2024-06-12 16:05:56 +0100

    Updated to v7.19.0

--- loginSignature.js
+++ loginSignature.js
@@ -6,7 +6,7 @@ const { constants, createHash, createCipheriv, publicEncrypt } = require("crypto
 // App Variables
 var publicKey = "-----BEGIN PUBLIC KEY-----\nMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCbcu5SxnaX7kKQlVrZGqUhcs90\nlM5sv9cRxM527+nvrUoVEXTndBZZRLqXttcpNNbwTFPBrvMLc2x8ro+yY4Zw77ic\n2060DU6mJk+jFX5xHG0nec4sxBRtDMevei1+XexHCGLvuBvFQTSMuwF287dbDYQr\nNv/uxGBwplF/QzD4gwIDAQAB\n-----END PUBLIC KEY-----";
 var remote = "remote-android";
-var appVersion = "5.2.7";
+var appVersion = "7.19.0";
 
 function reversePassword(password) {
     return (password === "") ? "" : reversePassword(password.substr(1)) + password.charAt(0);
@@ -15,7 +15,7 @@ function reversePassword(password) {
 function createSignature(username, password) {
     password = publicEncrypt({ key: publicKey, padding: constants.RSA_NO_PADDING }, Buffer.concat([Buffer.alloc(128 - Buffer.from(reversePassword(password)).length), Buffer.from(reversePassword(password))])).toString("hex");
 
-    var cipher = createCipheriv("aes-128-cbc", "86eaddbced659304", "A514868A05D3DB1D");
+    var cipher = createCipheriv("aes-128-cbc", "db948dc801ca4f91", "7A2898A8CF071B71");
     var signature = cipher.update(`${createHash("md5").update(`${remote}-#-${appVersion}-#-${username}##${password}`).digest("hex")}-#-${Date.now()}`, "utf8", "base64");
 
     return [password, signature += cipher.final("base64")];
@@ -27,23 +27,3 @@ console.log({
     "encryptedPassword": signature[0],
     "signature": signature[1]
 });
-
-/*
-
-    POST https://apps2.lovense.com/api/wear/genGtoken
-
-    Headers -
-
-        Content-Type: application/x-www-form-urlencoded; charset=utf-8
-        Ver: 5.2.7
-
-    Form Data -
-
-        password: createSignature("username", "password")[0]
-        ver: 5.2.7
-        signature: createSignature("username", "password")[1]
-        pf: remote-android
-        tag: login
-        email: username/email
-
-*/
